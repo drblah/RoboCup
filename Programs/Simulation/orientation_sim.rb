4c1be059014f01d65819bef6d37e0ce5df3f2d2e
Sebastian Damsgaard <sdamsg15@student.aau.dk>
2015-09-09 17:12:10 +0200

    Now the simulation uses the heading, which makes everything work much better.

--- Programs/Simulation/orientation_sim.rb
+++ Programs/Simulation/orientation_sim.rb
@@ -15,18 +15,14 @@ ENCODER_COUNT_REVOLUTION = 360
 ENCODER_SCALE_FACTOR = ((Math::PI*WHEELSIZE)/ENCODER_COUNT_REVOLUTION)
 WHEELSEPARATION = 15
 
-(0..100).each do |i|
-	posx = posx + displacement(i,i, ENCODER_SCALE_FACTOR) * Math::cos(heading + rotation(i, i*0.7, ENCODER_SCALE_FACTOR, WHEELSEPARATION)/2)
-	posy = posy + displacement(i,i, ENCODER_SCALE_FACTOR) * Math::sin(heading + rotation(i, i*0.7, ENCODER_SCALE_FACTOR, WHEELSEPARATION)/2)
+
+
+(0..200).each do |i|
+	disp = displacement(i,i*1.2, ENCODER_SCALE_FACTOR)
+	rot = rotation(i, i*1.2, ENCODER_SCALE_FACTOR, WHEELSEPARATION)
+	heading = heading + rot
+	posx = posx + disp * Math::cos(heading + rot/2)
+	posy = posy + disp * Math::sin(heading + rot/2)
 	puts "#{i},#{posx},#{posy}"
-end
 
-#puts "Reversing"
-
-(0..99).each do |i|
-	ir = 100-i
-
-	posx = posx + displacement(ir,ir, ENCODER_SCALE_FACTOR) * Math::cos(heading + rotation(ir, ir*0.7, ENCODER_SCALE_FACTOR, WHEELSEPARATION)/2)
-	posy = posy + displacement(ir,ir, ENCODER_SCALE_FACTOR) * Math::sin(heading + rotation(ir, ir*0.7, ENCODER_SCALE_FACTOR, WHEELSEPARATION)/2)
-	puts "#{i},#{posx},#{posy}"
 end
